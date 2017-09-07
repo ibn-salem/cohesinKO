fbdc6c620579d1b80df80b58aae2c2ba271967dd
ibn-salem <jonas.ibn-salem@gmx.de>
2017-09-07 17:09:25 +0200

    add function reciprocalOverlaps() for reciprocal overlap between GRanges

--- R/GRanges.functions.R
+++ R/GRanges.functions.R
@@ -18,3 +18,37 @@ getBoundaries <- function(gr){
   
   return(boundaryGR)
 }
+
+
+#' Finding overlapping genomic ranges by reciprocal overlap cutoff.
+#'
+#' Given two ranges of length l1 and l2 with overlapping length ol, the
+#' reciprocal overlap is defined as lo / max(l1, l2).
+#'
+#' @param query,subject A GRanges or GRangesList object. See findOverlaps() in
+#'   GenomicRanges package.
+#' @param fraction The minimal fraction of reciprocal overlap allowed between
+#'   ranges.
+#' @param ... Futher arguments passed to findOverlaps() function.
+#' @value An \code{\link[S4Vectors]{Hits}} object with indices of overlappin
+#'   ranges.
+reciprocalOverlaps <- function(query, subject, fraction = 0.5, ...){
+  
+  # get any overlap hit object
+  hits <- findOverlaps(query, subject, type = "any", ...)
+  
+  # compute width of the overlapping region
+  overlaps <- width(pintersect(query[queryHits(hits)], subject[subjectHits(hits)]))
+  
+  # get max length of query and subject for each overlap
+  maxLen <- apply(
+    cbind(width(query[queryHits(hits)]), width(subject[subjectHits(hits)])),
+    1,max)
+  
+  # compute fraction of overlap
+  fracOverlap <- overlaps / maxLen
+  
+  # filter for overlap >= threshold
+  return(hits[fracOverlap >= fraction])
+  
+}
